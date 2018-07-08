c773f8e1a5fd10c0460ddf10b65e8853d219e63f
chriskelsey <chriskelsey.net@gmail.com>
2018-07-07 23:07:21 -0700

    Commit for homework completion

---
+++ assets/js/app.js
@@ -0,0 +1,88 @@
+//Declare our global variables
+var player = 0;
+var name = '';
+var playerCount = 0;
+
+
+//Hide the game on load
+
+$(document).ready(function() {
+	$('#game').hide();
+
+});
+
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyApyCL1CrC21zLRnsia93t3zMiuzubsLvA",
+    authDomain: "rps-multiplayer-51100.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-51100.firebaseio.com",
+    projectId: "rps-multiplayer-51100",
+    storageBucket: "rps-multiplayer-51100.appspot.com",
+    messagingSenderId: "1089597714380"
+  };
+
+  firebase.initializeApp(config);
+  var database = firebase.database();
+
+  //Initialize initial keys
+
+  database.ref().on("child_added", function(childSnapshot){
+    player = childSnapshot.val().player;
+    name = childSnapshot.val().name;
+  })
+
+
+$('#start-button').on('click', function(e) {
+
+  var playerName = $("#player-name").val().trim();
+
+  //assign player number or hold
+  if(player === 1){
+    playerCount = 2;
+  } else if(player === 2){
+    playerCount = 0;
+  } else {
+    playerCount = 1;
+  }
+
+    var newPlayer = {
+      name: playerName,
+      player: playerCount,
+      wins: 0,
+      losses: 0
+    };
+
+  	// Uploads the user to the database
+    database.ref().push()
+  	database.ref().push(newPlayer);
+
+
+    // Remove input bar and start game
+    $('#game').show();
+    $('#start-game').hide();
+    $('.name').text(playerName);
+    $('.player').text(playerCount);
+    $('#pl' + playerCount + '> .card-header').text(playerName);
+
+  	//prevent page refresh
+	e.preventDefault();
+});
+
+$('button').on('click',function() {
+  var plnum = 0;
+  database.ref().on('value',function(snapshot) {
+    plnum = snapshot.val().player;
+    console.log(snapshot.val().player);
+  });
+  if ($(this).attr('id') === 'rock') {
+    console.log(plnum);
+    $('#pl' + plnum).find('.card-title').addClass('fas fa-hand-rock').text('');
+  } else if ($(this).attr('id') === 'paper') {
+    $('#pl' + plnum).find('.card-title').addClass('fas fa-hand-paper').text('');
+  } else if ($(this).attr('id') === 'scissors'){
+    $('#pl' + plnum).find('.card-title').addClass('fas fa-hand-scissors').text('');
+  } else {
+    return false;
+  }
+});
+
